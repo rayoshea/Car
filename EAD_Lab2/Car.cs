56951eb83f82dfc6ee72e0b9c8f4245b3747f2ed
rayray <raymoshea@gmail.com>
2015-02-10 12:55:26 +0000

    hello

---
+++ EAD_Lab2/Car.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EAD_Lab2
+{
+    class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string Reg { get; set; }
+        public int EngSize { get; set; }
+
+        public override string ToString()
+        {
+            return Make + " " + Model + " " + Reg + " " + EngSize;
+        }
+    }
+}
